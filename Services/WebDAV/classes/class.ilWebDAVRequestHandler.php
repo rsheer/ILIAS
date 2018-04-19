e7a8c86683437586e20f8d43b58a570168a98dd0
Raphael Heer <raphael.heer@hslu.ch>
2018-04-19 15:13:53 +0200

    WebDAV: Added class to handle WebDAV-Requests

---
+++ Services/WebDAV/classes/class.ilWebDAVRequestHandler.php
@@ -0,0 +1,77 @@
+<?php
+
+use function Sabre\HTTP\decodePath;
+
+require_once 'libs/composer/vendor/autoload.php';
+
+// Include all needed classes for a webdav-request
+include_once "Services/WebDAV/classes/auth/class.ilWebDAVAuthentication.php";
+include_once "Services/WebDAV/classes/dav/class.ilObjectDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilObjContainerDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilObjFileDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilObjCategoryDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilObjCourseDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilObjGroupDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilObjFolderDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilMountPointDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilClientNodeDAV.php";
+include_once "Services/WebDAV/classes/dav/class.ilObjRepositoryRootDAV.php";
+
+
+class ilWebDAVRequestHandler
+{
+    private static $instance;
+    
+    public static function getInstance()
+    {
+        return self::$instance ? self::$instance : self::$instance = new ilWebDAVRequestHandler();
+    }
+
+    public function handleRequest()
+    {
+        try
+        {
+            ilLoggerFactory::getLogger('WebDAV')->debug('New WebDAV Session');
+            $root_dir = $this->getRootDir();
+            
+            $server = new Sabre\DAV\Server($root_dir);
+            
+            $this->setPlugins($server);
+            
+            ilLoggerFactory::getLogger('WebDAV')->debug('Ready to execute server');
+            $server->exec();
+        }
+        // TODO: Remove after developement and debugging
+        catch(Exception $e)
+        {
+            file_put_contents('WebDAV_Exception.txt',  'Message: '.$e->getMessage()."\n", FILE_APPEND);
+            file_put_contents('WebDAV_Exception.txt',  'File: '.$e->getFile()."\n", FILE_APPEND);
+            file_put_contents('WebDAV_Exception.txt',  'Line: '.$e->getLine()."\n", FILE_APPEND);
+            file_put_contents('WebDAV_Exception.txt',  'Code: '.$e->getCode()."\n\n", FILE_APPEND);
+        }
+    }
+    
+    protected function getRootDir()
+    {
+        return new ilMountPointDAV();
+    }
+    
+    protected function setPlugins($server)
+    {
+        global $DIC;
+        
+         // Set browser plugin (used for testing)
+         $server->addPlugin(new Sabre\DAV\Browser\Plugin());
+
+         // Set authentication plugin
+         $webdav_auth = new ilWebDAVAuthentication();
+         $cal = new Sabre\DAV\Auth\Backend\BasicCallBack(array($webdav_auth, 'authenticate'));
+         $plugin = new Sabre\DAV\Auth\Plugin($cal);
+         $server->addPlugin($plugin);
+         
+         // TODO: Implement lock plugin. Code would look like this:
+         //$lock_backend = new ilWebDAVLockBackend();
+         //$lock_plugin = new Sabre\DAV\Locks\Plugin($lock_backend);
+         //$server->addPlugin($lock_plugin);
+    }
+}
