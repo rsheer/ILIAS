4af2f1d4957d5b64f18bd38d99548e448cd5398b
Raphael Heer <raphael.heer@hslu.ch>
2018-04-20 13:36:28 +0200

    WebDAV: Added include and a mocked function do the lock backend

--- Services/WebDAV/classes/lock/class.ilWebDAVLockBackend.php
+++ Services/WebDAV/classes/lock/class.ilWebDAVLockBackend.php
@@ -1,7 +1,12 @@
 <?php
+require_once 'libs/composer/vendor/autoload.php';
 
-
-class ilWebDAVLockBackend extends Sabre\DAV\Locks\Backend\AbstractBackend
+/**
+ * TODO: Implement this class
+ *
+ * @author faheer
+ */
+class ilWebDAVLockBackend extends sabre\DAV\Locks\Backend\AbstractBackend
 {
     /** @var $db ilDB */
     protected $db;
@@ -18,15 +23,27 @@ class ilWebDAVLockBackend extends Sabre\DAV\Locks\Backend\AbstractBackend
         
     }
 
-    public function unlock($uri, \Sabre\DAV\Locks\LockInfo $lockInfo)
+    public function unlock($uri, sabre\DAV\Locks\LockInfo $lockInfo)
     {
         
     }
 
-    public function lock($uri, \Sabre\DAV\Locks\LockInfo $lockInfo)
+    public function lock($uri, sabre\DAV\Locks\LockInfo $lockInfo)
     {
         
     }
 
     
+    /**
+     * For the moment just mocking, so ILIAS won't break. I will implement this later
+     *
+     * TODO: Implement this function
+     *
+     * @param int $obj_id
+     * @return array
+     */
+    public function getLocksOnObjectObj(int $obj_id)
+    {
+        return array();
+    }
 }
