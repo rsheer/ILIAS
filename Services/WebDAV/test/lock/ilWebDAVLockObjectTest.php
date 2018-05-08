e774cf0bda3f599b56fc1ae48d9d7def794ffb39
Raphael Heer <raphael.heer@hslu.ch>
2018-05-08 11:20:28 +0200

    WebDAV: Added test classes to try unit testing :-)

---
+++ Services/WebDAV/test/lock/ilWebDAVLockObjectTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Monolog\TestCase;
+
+class ilWebDAVLockObjectTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+    
+    public function testIfTestingWorks()
+    {
+        
+    }
+    
+}
